00a7c85f70967707f0888f543d5f78765d2e1a0d
shansreenivas <shansree1@gmail.com>
2024-04-27 22:53:23 +0200

    Initial commit

---
+++ src/main/java/com/hydra/divideup/DivideupApplication.java
@@ -0,0 +1,13 @@
+package com.hydra.divideup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivideupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivideupApplication.class, args);
+	}
+
+}
