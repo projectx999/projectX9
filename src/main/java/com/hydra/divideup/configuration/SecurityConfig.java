5830f4be0c7d54e801726578b26b7ac126d9999f
shansreenivas <shan.sreenivas@volvocars.com>
2024-04-30 17:29:48 +0200

    user

--- src/main/java/com/hydra/divideup/configuration/SecurityConfig.java
+++ src/main/java/com/hydra/divideup/configuration/SecurityConfig.java
@@ -2,14 +2,28 @@ package com.hydra.divideup.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests(authorize -> authorize
+            .anyRequest().permitAll())  // Permit all requests without requiring authentication
+        .csrf().disable()  // Optionally disable CSRF protection if it's not needed
+        .httpBasic().disable();  // Disable HTTP Basic authentication
+    return http.build();
   }
 
 }
