cacae0a0fd93317d5147c797c3e71e9651eb5c77
shansreenivas <shan.sreenivas@volvocars.com>
2024-05-11 20:31:34 +0200

    user

---
+++ src/main/java/com/hydra/divideup/entity/Payment.java
@@ -0,0 +1,26 @@
+package com.hydra.divideup.entity;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document
+public class Payment {
+
+      private String id;
+      private String userId;
+      private String groupId;
+      private String description;
+      private String paidBy;
+      private String currency;
+      private double amount;
+      private LocalDateTime date;
+      private String category;
+      private String note;
+      private boolean isSettled;
+      private String splitType; // change to enum
+
+}
