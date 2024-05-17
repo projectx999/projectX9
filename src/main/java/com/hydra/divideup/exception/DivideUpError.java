7fd44b02fcc0f0028fb4b18496daec7d9ac3599c
manji <manjit.in@outlook.com>
2024-05-17 05:15:37 +0200

    RestControllerAdvice for custom exceptions

--- src/main/java/com/hydra/divideup/exception/DivideUpError.java
+++ src/main/java/com/hydra/divideup/exception/DivideUpError.java
@@ -3,8 +3,9 @@ package com.hydra.divideup.exception;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @Getter
-@ToString
 public enum DivideUpError {
 
     USER_NOT_FOUND(1001,"User not found"),
@@ -12,8 +13,12 @@ public enum DivideUpError {
 
     private final int code;
     private final String message;
+    private LocalDateTime timestamp;
+
+
 
     DivideUpError(int code, String message) {
+        this.timestamp = LocalDateTime.now();
         this.code=code;
         this.message = message;
     }
