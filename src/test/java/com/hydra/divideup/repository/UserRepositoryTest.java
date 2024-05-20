04d3039319c7469bb9725bffa41d4bde6ebc3d0b
shansreenivas <shan.sreenivas@volvocars.com>
2024-05-20 13:15:30 +0200

    Repository test with embedded DB

--- src/test/java/com/hydra/divideup/repository/UserRepositoryTest.java
+++ src/test/java/com/hydra/divideup/repository/UserRepositoryTest.java
@@ -3,22 +3,23 @@ package com.hydra.divideup.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.hydra.divideup.entity.User;
-import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @DataMongoTest
-@ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 class UserRepositoryTest {
 
   @Autowired
   private UserRepository userRepository;
+
+  @BeforeEach
+  public void setUp() {
+    userRepository.deleteAll();
+  }
 
   @Test
   void whenSaveUser_thenUserCanBeRetrieved() {
@@ -28,9 +29,8 @@ class UserRepositoryTest {
     userRepository.save(user);
     User found = userRepository.findById(user.getId()).orElse(null);
     assertThat(found)
-            .isNotNull()
-            .returns("Shan",User::getName)
-            .returns("test@gmail.com",User::getEmail);
+        .isNotNull()
+        .returns("Shan", User::getName)
+        .returns("test@gmail.com", User::getEmail);
   }
-
 }
