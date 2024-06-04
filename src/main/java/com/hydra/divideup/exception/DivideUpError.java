8b530be7fe79fd2e0c050a0ecab7d00057ec0fe8
shansreenivas <shan.sreenivas@volvocars.com>
2024-06-04 15:56:22 +0200

    user validation and pr comments fix

--- src/main/java/com/hydra/divideup/exception/DivideUpError.java
+++ src/main/java/com/hydra/divideup/exception/DivideUpError.java
@@ -1,25 +1,23 @@
 package com.hydra.divideup.exception;
 
-import lombok.Getter;
-import lombok.ToString;
-
 import java.time.LocalDateTime;
+import lombok.Getter;
 
 @Getter
 public enum DivideUpError {
 
-    USER_NOT_FOUND(1001,"User not found"),
-    USER_ALREADY_EXISTS(1002,"User with emailId or phoneNumber already exists");
+  USER_NOT_FOUND(1001, "User not found"),
+  USER_PHONE_EXISTS(1002, "User with phoneNumber already exists"),
+  USER_EMAIL_EXISTS(1003, "User with emailId already exists");
 
-    private final int code;
-    private final String message;
-    private LocalDateTime timestamp;
+  private final int code;
+  private final String message;
+  private LocalDateTime timestamp;
 
 
-
-    DivideUpError(int code, String message) {
-        this.timestamp = LocalDateTime.now();
-        this.code=code;
-        this.message = message;
-    }
+  DivideUpError(int code, String message) {
+    this.timestamp = LocalDateTime.now();
+    this.code = code;
+    this.message = message;
+  }
 }
