c661425150d088d1a0af50956c81102308673dda
manji <manjit.in@outlook.com>
2024-06-06 13:50:08 +0200

    PX6: test group>getGroupByUser,uer>getUsers

--- src/test/java/com/hydra/divideup/controller/UserControllerTest.java
+++ src/test/java/com/hydra/divideup/controller/UserControllerTest.java
@@ -1,20 +1,21 @@
 package com.hydra.divideup.controller;
 
 import com.hydra.divideup.entity.User;
-import com.hydra.divideup.exception.DivideUpError;
 import com.hydra.divideup.exception.RecordNotFoundException;
 import com.hydra.divideup.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.function.Supplier;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static com.hydra.divideup.exception.DivideUpError.USER_NOT_FOUND;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -42,12 +43,12 @@ class UserControllerTest {
         when(userService.getUser(id)).thenReturn(user);
 
         //then
-        mockMvc.perform(get(usersUrl+"/{id}", id))
+        mockMvc.perform(get(usersUrl + "/{id}", id))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(id)))
-                .andExpect(jsonPath("$.name", is(user.getName())))
                 .andExpect(jsonPath("$.email", is(user.getEmail())))
-                //make sure password not revealed
+                .andExpect(jsonPath("$.phoneNumber",is(user.getPhoneNumber())))
+                //make sure password not revealed outside
                 .andExpect(jsonPath("$.password").doesNotExist());
 
         //and
@@ -62,11 +63,57 @@ class UserControllerTest {
         when(userService.getUser(id)).thenThrow(new RecordNotFoundException(USER_NOT_FOUND));
 
         //then
-        mockMvc.perform(get(usersUrl + "/{id}",id))
+        mockMvc.perform(get(usersUrl + "/{id}", id))
                 .andExpect(status().isInternalServerError())
-                .andExpect(jsonPath("$.code",is(USER_NOT_FOUND.getCode())))
-                .andExpect(jsonPath("$.message",is(USER_NOT_FOUND.getMessage())));
-        verify(userService,times(1)).getUser(id);
+                .andExpect(jsonPath("$.code", is(USER_NOT_FOUND.getCode())))
+                .andExpect(jsonPath("$.message", is(USER_NOT_FOUND.getMessage())));
+        //And
+        verify(userService, times(1)).getUser(id);
+
+    }
+
+    @Test
+    void testGetUsers() throws Exception {
+        User user1 = new User("manji@gmail", "123456789", "pass@123");
+        user1.setId("123");
+
+        User user2 = new User("ranji@gmail", "678912345", "check@123");
+        user2.setId("234");
+
+        List<User> users = List.of(user1, user2);
+
+        //when
+        when(userService.getUsers()).thenReturn(users);
+
+        //then
+        mockMvc.perform(get(usersUrl))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                //user1
+                .andExpect(jsonPath("$[0].id", is(user1.getId())))
+                .andExpect(jsonPath("$[0].email", is(user1.getEmail())))
+                .andExpect(jsonPath("$[0].phoneNumber", is(user1.getPhoneNumber())))
+                //make sure password not revealed outside
+                .andExpect(jsonPath("$[0].password").doesNotExist())
+                //user2
+                .andExpect(jsonPath("$[1].id", is(user2.getId())))
+                .andExpect(jsonPath("$[1].email", is(user2.getEmail())))
+                .andExpect(jsonPath("$[1].phoneNumber", is(user2.getPhoneNumber())))
+                //make sure password not revealed outside
+                .andExpect(jsonPath("$[1].password").doesNotExist());
+        //and
+        verify(userService, times(1)).getUsers();
+
+    }
+
+    @Test
+    void testGetUsers_EmptyList() throws Exception {
+        //when
+        when(userService.getUsers()).thenReturn(Collections.emptyList());
+        //then
+        mockMvc.perform(get(usersUrl))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(0)));
 
     }
 
