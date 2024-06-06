9167b5a934072edc18c32627b7698094579d6be2
manji <manjit.in@outlook.com>
2024-06-06 03:03:49 +0200

    PX6 getMapping tests for user,group

--- src/test/java/com/hydra/divideup/controller/UserControllerTest.java
+++ src/test/java/com/hydra/divideup/controller/UserControllerTest.java
@@ -1,33 +1,74 @@
 package com.hydra.divideup.controller;
 
+import com.hydra.divideup.entity.User;
+import com.hydra.divideup.exception.DivideUpError;
+import com.hydra.divideup.exception.RecordNotFoundException;
+import com.hydra.divideup.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(UserControllerTest.class)
+import java.util.function.Supplier;
+
+import static com.hydra.divideup.exception.DivideUpError.USER_NOT_FOUND;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class})
 class UserControllerTest {
 
-  @Autowired private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @Test
-  void getUsers() {}
+    @MockBean
+    private UserService userService;
 
-  @Test
-  void createUser() {}
+    private final String usersUrl = "/api/v1/users";
 
-  @Test
-  void getUser() {}
+    @Test
+    void tetGetUser() throws Exception {
+        final String id = "123";
+        User user = new User("manji@gmail", "123456789", "pass@123");
+        user.setId(id);
 
-  @Test
-  void updateUser() {}
+        //when
+        when(userService.getUser(id)).thenReturn(user);
 
-  @Test
-  void blockUser() {}
+        //then
+        mockMvc.perform(get(usersUrl+"/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(id)))
+                .andExpect(jsonPath("$.name", is(user.getName())))
+                .andExpect(jsonPath("$.email", is(user.getEmail())))
+                //make sure password not revealed
+                .andExpect(jsonPath("$.password").doesNotExist());
 
-  @Test
-  void unblockUser() {}
+        //and
+        verify(userService, times(1)).getUser(id);
+    }
 
-  @Test
-  void deleteUser() {}
+    @Test
+    void testGetUser_NotFound() throws Exception {
+        final String id = "234";
+
+        //when
+        when(userService.getUser(id)).thenThrow(new RecordNotFoundException(USER_NOT_FOUND));
+
+        //then
+        mockMvc.perform(get(usersUrl + "/{id}",id))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.code",is(USER_NOT_FOUND.getCode())))
+                .andExpect(jsonPath("$.message",is(USER_NOT_FOUND.getMessage())));
+        verify(userService,times(1)).getUser(id);
+
+    }
+
 }
+
