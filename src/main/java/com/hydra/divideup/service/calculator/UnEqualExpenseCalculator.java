0c41e7b0ac72a446ea02ccce22b42f1841d19a2d
manji <manjit.in@outlook.com>
2024-07-16 22:44:46 +0200

    PX34: Unequal Payment

--- src/main/java/com/hydra/divideup/service/calculator/UnEqualExpenseCalculator.java
+++ src/main/java/com/hydra/divideup/service/calculator/UnEqualExpenseCalculator.java
@@ -2,7 +2,10 @@ package com.hydra.divideup.service.calculator;
 
 import com.hydra.divideup.entity.Expense;
 import com.hydra.divideup.entity.Payment;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +13,25 @@ public final class UnEqualExpenseCalculator extends ExpenseCalculator {
 
   @Override
   protected List<Expense> calculateExpensesForGroupExpense(Payment payment) {
-    return List.of();
+    var amount = BigDecimal.valueOf(payment.getAmount());
+    var splitDetails = payment.getSplitDetails();
+
+    List<Expense> expenses =
+        payment.getSplitDetails().entrySet().stream()
+            .filter(member -> !member.getKey().equals(payment.getPaidBy()))
+            .map(
+                member ->
+                    new Expense(
+                        payment, member.getKey(), BigDecimal.valueOf(member.getValue()).negate()))
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    expenses.add(
+        new Expense(
+            payment,
+            payment.getPaidBy(),
+            amount.subtract(
+                BigDecimal.valueOf(splitDetails.getOrDefault(payment.getPaidBy(), 0.0)))));
+    return expenses;
   }
 
   @Override
