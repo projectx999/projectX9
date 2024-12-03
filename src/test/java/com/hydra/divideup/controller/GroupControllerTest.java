62d815699481ab7305760e786ecd0ec83c3db90a
shansreenivas <shan.sreenivas@volvocars.com>
2024-12-03 04:38:17 +0100

    bumb version

--- src/test/java/com/hydra/divideup/controller/GroupControllerTest.java
+++ src/test/java/com/hydra/divideup/controller/GroupControllerTest.java
@@ -34,8 +34,8 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(
@@ -45,7 +45,7 @@ class GroupControllerTest {
 
   @Autowired private MockMvc mockMvc;
 
-  @MockBean private GroupService groupService;
+  @MockitoBean private GroupService groupService;
 
   @Autowired ObjectMapper objectMapper;
 
